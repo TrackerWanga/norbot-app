48ee6579880c21b28a61ec5d61502a8385eb2996
TrackerWanga <trackerwanga@gmail.com>
2026-06-13 14:10:43 +0000

    fix: Add gradle wrapper JAR and fix gradlew script

---
+++ generate_wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Download gradle wrapper jar
+curl -sL https://raw.githubusercontent.com/gradle/gradle/v8.4.0/gradle/wrapper/gradle-wrapper.jar -o gradle/wrapper/gradle-wrapper.jar
+echo "Wrapper JAR downloaded"
